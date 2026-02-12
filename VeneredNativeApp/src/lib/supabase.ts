109ef7a36216d0468cb3739acf36a382dd39dda6
vivenesjuan332 <vivenesjuan332@gmail.com>
2026-02-12 22:06:55 +0000

    chore: Force remove submodule tracking

--- VeneredNativeApp/src/lib/supabase.ts
+++
@@ -1,8 +0,0 @@
-
-import 'react-native-url-polyfill/auto';
-import { createClient } from '@supabase/supabase-js';
-
-const supabaseUrl = 'https://cipcpgpbjimnhesuerrk.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNpcGNwZ3Biamltbmhlc3VlcnJrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTY0NTg0MjgsImV4cCI6MjAzMjAzNDQyOH0.sb_publishable_HLa6pQB2-9YWv9ILf_IfKg_XLC_g9jT';
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
