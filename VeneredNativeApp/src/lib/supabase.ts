f5930427db76e50777994d495eb107c876155854
vivenesjuan332 <vivenesjuan332@gmail.com>
2026-02-13 11:01:33 +0000

    refactor: Secure Supabase credentials using .env file

--- VeneredNativeApp/src/lib/supabase.ts
+++ VeneredNativeApp/src/lib/supabase.ts
@@ -1,8 +1,11 @@
-
 import 'react-native-url-polyfill/auto';
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://cipcpgpbjimnhesuerrk.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNpcGNwZ3Biamltbmhlc3VlcnJrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTY0NTg0MjgsImV4cCI6MjAzMjAzNDQyOH0.sb_publishable_HLa6pQB2-9YWv9ILf_IfKg_XLC_g9jT';
+const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL;
+const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY;
+
+if (!supabaseUrl || !supabaseAnonKey) {
+  throw new Error('Supabase URL and Anon Key must be provided.');
+}
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
