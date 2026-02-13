e356e56e35a08a186b518a3286f5f8610d7249f5
vivenesjuan332 <vivenesjuan332@gmail.com>
2026-02-13 21:20:10 +0000

    fix: Add required config for react-native-screens

--- VeneredNativeApp/android/app/src/main/java/com/venerednativeapp/MainActivity.kt
+++ VeneredNativeApp/android/app/src/main/java/com/venerednativeapp/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.venerednativeapp
 
+import android.os.Bundle;
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
 import com.facebook.react.defaults.DefaultReactActivityDelegate
 
 class MainActivity : ReactActivity() {
+
+  // --- INICIO DE LA MODIFICACIÓN REQUERIDA ---
+  // Esto es requerido por react-native-screens para evitar crashes en builds de release.
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(null)
+  }
+  // --- FIN DE LA MODIFICACIÓN REQUERIDA ---
 
   /**
    * Returns the name of the main component registered from JavaScript. This is used to schedule
